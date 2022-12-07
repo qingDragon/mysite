2555a0f0675897f75aedef04a5bf892d20b1f405
qingDragon <921971961@qq.com>
2022-12-07 18:33:33 +0800

    Initial commit

---
+++ app01/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from app01 import models
+# Create your views here.
+def depart_list(request):
+    """ 部门列表 """
+    # 去数据库中获取所有的部门列表
+    # [对象，对象，对象]
+    queryset = models.Department.objects.all()
+    return render(request, "depart_list.html",{'queryset':queryset})
